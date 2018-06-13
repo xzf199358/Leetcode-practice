681bc502486ceb7f6695b70087479b0a227dbb90
ZiFeng Xu <32630364+xzf199358@users.noreply.github.com>
2018-06-13 16:59:07 +0800

    Update 06_z字形变换.py

--- 06_z字形变换.py
+++ 06_z字形变换.py
@@ -1,3 +1,11 @@
+'''
+题目:  将字符串 "PAYPALISHIRING" 以Z字形排列成给定的行数：
+示例：P   A   H   N
+     A P L S I I G
+     Y   I   R
+    之后从左往右，逐行读取字符："PAHNAPLSIIGYIR"
+
+'''
 #coding=UTF-8
 class Solution:
     def convert(self,s,numRows):
